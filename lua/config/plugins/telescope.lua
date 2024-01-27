4376aede79b3c753458c489ec2d00f672bb82b98
ink0rr <tomflynnbeltsazar@gmail.com>
2024-01-27 17:23:47 +0700

    Update telescope.nvim

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,39 +1,11 @@
-local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == "" then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ":h")
-  end
-
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    print("Not a git repository. Searching on current working directory")
-    return cwd
-  end
-  return git_root
-end
-
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.4",
+  tag = "0.1.5",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader><space>", builtin.buffers)
     vim.keymap.set("n", "<leader>p", builtin.find_files)
     vim.cmd.cd("%:p:h")
-    local git_root = find_git_root()
-    if git_root then
-      vim.cmd.cd(git_root)
-    else
-      vim.cmd.cd("%:p:h")
-    end
   end,
 }
